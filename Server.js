f3f6e4fb9be8757447a0bfab16643294e71e11bb
Naitneg <105953191+Naitneg@users.noreply.github.com>
2022-08-09 16:54:48 +0200

    updating server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,28 @@
-   
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.post("/signin", (req, res) => {
+  res.json("Working");
+});
+
+app.post("/register", (req, res) => {
+  res.json("Working");
+});
+
+app.listen(3000);
+
+//  QITO JON PATH QE DUHET ME I KRIJU E TANI ME I LIDH ME FRONT-END
+//  BONE NIHER NPM INSTALL PER ME I INSTALU KTO dependencies QKA NA VYN
+// /signin --> POST success/fail
+// /register --> POST = user
+// /profile/:userID --> GET = user
+// /dashboard --> GET = admin
