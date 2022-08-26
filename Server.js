e3496a8fe763df5f4ac95be7b72af7f31d9d716b
Naitneg <105953191+Naitneg@users.noreply.github.com>
2022-08-26 04:20:29 +0200

    upd

--- Server.js
+++ Server.js
@@ -10,11 +10,13 @@ app.use(bodyParser.json());
 
 const db = knex({
   client: "pg",
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+client.connect();
 app.use(cors());
 
 app.get("/", (req, res) => {
