405eba008494cf39c0ce0d03cdb63c2a8188a87b
Naitneg <105953191+Naitneg@users.noreply.github.com>
2022-08-26 03:33:34 +0200

    database

--- Server.js
+++ Server.js
@@ -11,10 +11,8 @@ app.use(cors());
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "123",
-    database: "movieapp_database",
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
