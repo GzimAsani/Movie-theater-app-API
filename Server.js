7043ac6dceb18023795879cc1d8fbaa8cb5c897b
Naitneg <105953191+Naitneg@users.noreply.github.com>
2022-08-26 21:41:27 +0200

    no

--- Server.js
+++ Server.js
@@ -123,7 +123,7 @@ app.post("/register", (req, res) => {
         })
         .then(trx.commit)
         .catch(trx.rollback);
-    }).catch((err) => res.status(400).json("1unable to register"));
+    }).catch((err) => res.status(400).json("unable to register"));
   });
 });
 
