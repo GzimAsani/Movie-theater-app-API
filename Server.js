a5ae6ded1732aa35ca456147d5c70283f243f46f
Naitneg <105953191+Naitneg@users.noreply.github.com>
2022-08-27 03:15:47 +0200

    :asd

--- Server.js
+++ Server.js
@@ -67,7 +67,7 @@ app.post("/movies", (req, res) => {
     .into("movies")
     .then((user) => {
       res.json(user[0]);
-    });
+    }).catch(err => {res.status(400).json("erorr")});
 });
 
     
