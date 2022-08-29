b6703ca07a08465a1edaaa5ecc99529fd8cf7187
Naitneg <105953191+Naitneg@users.noreply.github.com>
2022-08-30 00:52:24 +0200

    asd

--- Server.js
+++ Server.js
@@ -17,7 +17,7 @@ const db = knex({
 app.use(cors());
 
 app.get("/", (req, res) => {
- res.send(db.users);
+ res.json(db.users);
 });
 app.get("/users", (req,res) => {
   db.select("*").from("users").then(user => {
