5202a7addd5f322d898cd7b0ef1ecae0420f4c70
Naitneg <105953191+Naitneg@users.noreply.github.com>
2022-08-30 18:56:33 +0200

    asd

--- Server.js
+++ Server.js
@@ -92,7 +92,8 @@ app.post("/login", (req, res) => {
           .from("users")
           .where("username", "=", req.body.username)
           .then((user) => {
-            res.json(user[0]);
+            const accessToken = jwt.sign(user[0], process.env.ACCESS_TOKEN_SECRET)
+            res.json({accessToken: accessToken});
           })
           .catch((err) => res.status(400).json("unable to get user"));
       } else {
